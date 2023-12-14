61c7c3094a259ae02687cf59d01790bae44a05ef
longxiaofei <libra.19951002@gmail.com>
2023-12-14 21:28:46 +0800

    chore: bump to v0.1.38

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,4 +4,4 @@ from gw_dsl_parser import get_sql_from_payload
 def test_core_function():
     payload = {"workflow": [{"type": "view", "query": [{"op": "aggregate", "groupBy": [], "measures": [{"field": "*", "agg": "count", "asFieldKey": "count"}]}]}]}
     table_name = "test_table"
-    assert get_sql_from_payload(table_name, payload) == "SELECT count(*) AS \"count\" FROM \"test_table\""
+    assert get_sql_from_payload(table_name, payload) == "SELECT count(*) AS \"count\" FROM (SELECT * FROM \"test_table\") AS \"k6s_base_table\""
